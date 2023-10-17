59eb323c57ac2e0dfb4c32aed09fa7b310b2d9f8
krasnogurskiy <andrey.kranogurskiy@gmail.com>
2023-10-17 13:05:46 +0300

    add select method

--- DBinit/DataBase.cs
+++ DBinit/DataBase.cs
@@ -1,33 +1,60 @@
-﻿using Npgsql;
+﻿	using Npgsql;
 using System.Data.Common;
 
 namespace DBinit
 {
-	public class DataBase
-	{
-		public NpgsqlConnection connection;
+    public class DataBase
+    {
+        public NpgsqlConnection connection;
 
-		public DataBase(string connectionString)
-		{
-			this.connection = new NpgsqlConnection(connectionString);
-		}
+        public DataBase(string connectionString)
+        {
+            this.connection = new NpgsqlConnection(connectionString);
+        }
 
-		public void ExecuteNonQuery(string query)
-		{
-			try
-			{
-				connection.Open();
-				NpgsqlCommand command = new(query, connection);
-				command.ExecuteNonQuery();
-			}
-			catch (DbException ex)
-			{
-				Console.WriteLine("Database error occured: " + ex.Message);
-			}
-			finally
-			{
-				connection.Close();
-			}
-		}
-	}
+        public void ExecuteNonQuery(string query)
+        {
+            try
+            {
+                connection.Open();
+                NpgsqlCommand command = new(query, connection);
+                command.ExecuteNonQuery();
+            }
+            catch (DbException ex)
+            {
+                Console.WriteLine("Database error occured: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public void PrintTableData(string tableName)
+        {
+            try
+            {
+                connection.Open();
+                var command = new NpgsqlCommand($"SELECT * FROM {tableName}", connection);
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; ++i)
+                    {
+                        Console.Write(reader.GetFieldValue<object>(i) + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (DbException ex)
+            {
+                Console.WriteLine($"Database error occurred: {ex.Message}");
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
 }
--- DBinit/Program.cs
+++ DBinit/Program.cs
@@ -7,10 +7,19 @@ namespace DBinit
 	{
 		static void Main(string[] args)
 		{
-			string connectionString = "Host=surus.db.elephantsql.com;Username=ezzaidoo;Password=VvgDIad5Osco_z1erVN-xhbJ3xuqdBCZ;Database=ezzaidoo;";
-			DataBase db = new DataBase(connectionString);
-			DbInitTask.CreateTables(db);
-			//DbInitTask.FillUsersTable(db);
-		}
+            string connectionString = "Host=surus.db.elephantsql.com;Username=ezzaidoo;Password=VvgDIad5Osco_z1erVN-xhbJ3xuqdBCZ;Database=ezzaidoo;";
+            DataBase db = new DataBase(connectionString);
+
+    
+
+            //db.PrintTableData("items");
+            //db.PrintTableData("order_item");
+            //db.PrintTableData("orders");
+            //db.PrintTableData("payments");
+            //db.PrintTableData("subscription");
+            //db.PrintTableData("users");
+            //db.PrintTableData("users_subscriptions");
+        
+        }
 	}
 }
