fd807f5910519e0f5a7309464e08ceef3347bb98
krasnogurskiy <andrey.kranogurskiy@gmail.com>
2023-10-31 14:00:40 +0200

    add 2 windows

---
+++ ThreeLayerArchitecture/TradePulse/Presentation/Orders.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Presentation
+{
+    /// <summary>
+    /// Interaction logic for Orders.xaml
+    /// </summary>
+    public partial class Orders : Window
+    {
+        public Orders()
+        {
+            InitializeComponent();
+        }
+
+        private void CategoriesButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+    }
+}
